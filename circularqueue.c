be0c2b438c599f76e1d81ebf891fc477718e9701
Lakshay Arora <72307080+LakshayArora05@users.noreply.github.com>
2021-12-06 19:43:46 +0530

    circular queue

---
+++ circularqueue.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include<stdlib.h>
+#define max 6
+int queue[max];
+int front=-1;
+int rear=-1;
+void enqueue(int element)
+{
+if(front==-1 && rear==-1)
+{
+front=0;
+rear=0;
+queue[rear]=element;
+}
+else if((rear+1)%max==front)
+{
+printf("Queue is overflow..");
+}
+else
+{
+rear=(rear+1)%max;
+queue[rear]=element;
+}
+}
+int dequeue()
+{
+if((front==-1) && (rear==-1))
+{
+printf("\nQueue is underflow..");
+}
+else if(front==rear)
+{
+printf("\nThe dequeued element is %d", queue[front]);
+front=-1;
+rear=-1;
+}
+else
+{
+printf("\nThe dequeued element is %d", queue[front]);
+front=(front+1)%max;
+}
+}
+int main()
+{
+int choice=1,x;
+while(choice<4 && choice!=0)
+{
+30
+printf("\n Press 1: Insert an element");
+printf("\nPress 2: Delete an element");
+printf("\nPress 3: exit");
+printf("\nEnter your choice");
+scanf("%d", &choice);
+switch(choice)
+{
+case 1:
+printf("Enter the element which is to be inserted");
+scanf("%d", &x);
+enqueue(x);
+break;
+case 2:
+dequeue();
+break;
+case 3:
+exit(0);
+break;
+}}
+return 0;
+}
